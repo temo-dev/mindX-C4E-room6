75fa83613a9d9a314a19504b4a0501feb7aebbd3
Hanhhoang-jenny <hanhhoang2607@gmail.com>
2023-04-19 11:47:38 +0700

    taskjs-8

--- hanhhoang.js
+++ hanhhoang.js
@@ -1,37 +1,42 @@
-function demSoLe(number) {
-    let tongSoLe = 0
-    for (let i = 0; i < number; i++) {
-        if (i % 2 != 0) {
-            tongSoLe++
-        }
-    }
-    return `có tất cả ${tongSoLe} số lẻ của số nguyên dương ${number}`
-}
-
-// chương trình giải phương trình bật nhất ax + b = 0
-function phuongTrinhBatNhat (a,b) {
-    let x;
-    if (a == 0) {
-        if (b == 0) {
-            return `Phương trình có vô số nghiệm`
-        } else {
-            return `Phương trình vô nghiệm`
-        }
+// Viết chương trình giải phương trình bật 2 ax2 + bx + c = 0 
+function giaiPhuongTrinhBat2 (a,b,c) {
+    let x1;
+    let x2;
+    let delta = b ** 2 - 4 * a * c;
+    if (delta < 0) {
+        return `Phương trình vô nghiệm`
+    } if (delta == 0) {
+        x1 = x2 = -(b / 2 * a)
+        return `Phương trình có nghiệm kép x1 = x2 = ${x1}`
     } else {
-        x = -b / a
-        return `Phương trình có 1 nghiệm là x = ${x}` 
+        x1 = (-b - Math.sqrt(delta)) / 2 * a
+        x2 = (-b + Math.sqrt(delta)) / 2 * a
+        return `Phương trình có 2 nghiệm là x1 = ${x1} và x2 = ${x2}`
     }
 }
- function soNguyenDuong (number) {
-    let sumSoLe = 0
-    for (i = 0; i < number; i++) {
-        if  (i % 2 !== 0) {
-            sumSoLe += i
-        }
+console.log(giaiPhuongTrinhBat2(1,2,1)); 
+
+
+
+// Viết chương trình số lớn nhất a,b,c
+function soLonNhat(a,b,c) {
+    let maxNumber = Math.max(a,b,c);
+    return `Số lớn nhất là ${maxNumber}`
+}
+    console.log(soLonNhat(1,3,2));
+
+
+
+// Viết phương trình nhập 3 cạnh của tam giác, biết đó là tam giác gì
+function tamGiac(a,b,c) {
+    if (a === b && b === c) {
+        return `Tam giác đều`
+    } if (a === b || b === c || a === c) {
+        return `Tam giác cân`
+    } if (a !== b && b !== c) {
+        return `Tam giác thường`
+    } else {
+        return `Không phải tam giác`
     }
-    return `trong ${number} số nguyên dương thì tổng số lẻ là ${sumSoLe}`
- }
- console.log(soNguyenDuong(10))
-
-
-// 1 2 3 4 5 6 7 8 9 10
+}
+console.log(tamGiac(4,4,4));
